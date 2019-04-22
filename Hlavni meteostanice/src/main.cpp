cf3fe6dcaa2a3f9dd41ee6aa5cd10e7bb19bf770
Martin Pomykal <martin@pomykal.cz>
2019-04-22 20:03:41 +0200

    uprava promenych a pridaní teplomeru ve 2metrech

--- Hlavni meteostanice/src/main.cpp
+++ Hlavni meteostanice/src/main.cpp
@@ -31,13 +31,13 @@ OneWire oneWireDS(pinCidlaDS); // vytvoření instance oneWireDS z knihovny OneW
 DallasTemperature senzoryDS(&oneWireDS); // vytvoření instance senzoryDS z knihovny DallasTemperature
 // adresy 1-wire čidel//
 
- DeviceAddress tempSenzor100cm = {0x28, 0xAB, 0xA0, 0x77, 0x91, 0x11, 0x02, 0xB0};
- DeviceAddress tempSenzor50cm = {0x28, 0x89, 0x92, 0x77, 0x91, 0x11, 0x02, 0x6F};
- DeviceAddress tempSenzor20cm = {0x28, 0xA7, 0xE8, 0x77, 0x91, 0x09, 0x02, 0x80};
- DeviceAddress tempSenzor10cm = {0x28, 0xB9, 0x77, 0x77, 0x91, 0x14, 0x02, 0x75};
- DeviceAddress tempSenzor5cm = {0x28, 0xF4, 0xD0, 0x77, 0x91, 0x09, 0x02, 0x4D};
- DeviceAddress tempSenzorPrizemni5cm = {0x28, 0x30, 0xA4, 0x45, 0x92, 0x07, 0x02, 0x0B};
-
+DeviceAddress Senzor100cm = {0x28, 0xAB, 0xA0, 0x77, 0x91, 0x11, 0x02, 0xB0};
+DeviceAddress Senzor50cm = {0x28, 0x89, 0x92, 0x77, 0x91, 0x11, 0x02, 0x6F};
+DeviceAddress Senzor20cm = {0x28, 0xA7, 0xE8, 0x77, 0x91, 0x09, 0x02, 0x80};
+DeviceAddress Senzor10cm = {0x28, 0xB9, 0x77, 0x77, 0x91, 0x14, 0x02, 0x75};
+DeviceAddress Senzor5cm = {0x28, 0xF4, 0xD0, 0x77, 0x91, 0x09, 0x02, 0x4D};
+DeviceAddress SenzorPrizemni5cm = {0x28, 0x30, 0xA4, 0x45, 0x92, 0x07, 0x02, 0x0B};
+DeviceAddress Senzor200cm = {0x28, 0x32, 0xD9, 0x35, 0x05, 0x00, 0x00, 0xCC};
 /*---------Proměnné-------------------------*/
 
 float temp100cm;
@@ -46,6 +46,7 @@ float temp20cm;
 float temp10cm;
 float temp5cm;
 float tempPrizemni5cm;
+float temp200cm;
 
 void setup() {
 
@@ -55,13 +56,14 @@ void setup() {
   Serial.begin(115200);
   // zapnutí komunikace knihovny s teplotním čidlem
   senzoryDS.begin();
-  senzoryDS.setResolution(tempSenzor100cm, 10);
-  senzoryDS.setResolution(tempSenzor50cm, 10);
-  senzoryDS.setResolution(tempSenzor20cm, 10);
-  senzoryDS.setResolution(tempSenzor10cm, 10);
-  senzoryDS.setResolution(tempSenzor5cm, 10);
-  senzoryDS.setResolution(tempSenzorPrizemni5cm, 10);
-  
+  senzoryDS.setResolution(Senzor100cm, 10);
+  senzoryDS.setResolution(Senzor50cm, 10);
+  senzoryDS.setResolution(Senzor20cm, 10);
+  senzoryDS.setResolution(Senzor10cm, 10);
+  senzoryDS.setResolution(Senzor5cm, 10);
+  senzoryDS.setResolution(SenzorPrizemni5cm, 10);
+  senzoryDS.setResolution(Senzor200cm, 10);
+
   Wire.begin(); //inicializace I2C sběrnice
 
   WiFi.begin(ssid, password); // wifi s heslem
@@ -102,19 +104,21 @@ void loop ()
     /* 1-wire sekce */ // načtení informací ze všech čidel na daném pinu dle adresy a uložení do promněných
     if (millis() > PosledniTemp + CasNacteniTeploty * 1000)
     {
-      senzoryDS.requestTemperaturesByAddress(tempSenzor100cm);
-      senzoryDS.requestTemperaturesByAddress(tempSenzor50cm);
-      senzoryDS.requestTemperaturesByAddress(tempSenzor20cm);
-      senzoryDS.requestTemperaturesByAddress(tempSenzor10cm);
-      senzoryDS.requestTemperaturesByAddress(tempSenzor5cm);
-      senzoryDS.requestTemperaturesByAddress(tempSenzorPrizemni5cm);
+      senzoryDS.requestTemperaturesByAddress(Senzor100cm);
+      senzoryDS.requestTemperaturesByAddress(Senzor50cm);
+      senzoryDS.requestTemperaturesByAddress(Senzor20cm);
+      senzoryDS.requestTemperaturesByAddress(Senzor10cm);
+      senzoryDS.requestTemperaturesByAddress(Senzor5cm);
+      senzoryDS.requestTemperaturesByAddress(SenzorPrizemni5cm);
+      senzoryDS.requestTemperaturesByAddress(Senzor200cm);
 
-      temp100cm = senzoryDS.getTempC(tempSenzor100cm);
-      temp50cm = senzoryDS.getTempC(tempSenzor50cm);
-      temp20cm = senzoryDS.getTempC(tempSenzor20cm);
-      temp10cm = senzoryDS.getTempC(tempSenzor10cm);
-      temp5cm = senzoryDS.getTempC(tempSenzor5cm);
-      tempPrizemni5cm = senzoryDS.getTempC(tempSenzorPrizemni5cm);
+      temp100cm = senzoryDS.getTempC(Senzor100cm);
+      temp50cm = senzoryDS.getTempC(Senzor50cm);
+      temp20cm = senzoryDS.getTempC(Senzor20cm);
+      temp10cm = senzoryDS.getTempC(Senzor10cm);
+      temp5cm = senzoryDS.getTempC(Senzor5cm);
+      tempPrizemni5cm = senzoryDS.getTempC(SenzorPrizemni5cm);
+      temp200cm = senzoryDS.getTempC(Senzor200cm);
 
       Serial.println("[Načtení teploty z čidel]");
       PosledniTemp = millis();
@@ -130,10 +134,12 @@ void loop ()
       String url4 = "&Teplota_10=";
       String url5 = "&Teplota_5=";
       String url6 = "&Teplota_prizemni=";
+      String url7 = "&Teplota_200=";
+
 
       String host = "pomykal.eu";
 
-      client.print(String("GET ") + url + url1 + temp100cm + url2 + temp50cm + url3 + temp20cm + url4 + temp10cm + url5 + temp5cm + url6 + tempPrizemni5cm + " HTTP/1.1\r\n" +
+      client.print(String("GET ") + url + url1 + temp100cm + url2 + temp50cm + url3 + temp20cm + url4 + temp10cm + url5 + temp5cm + url6 + tempPrizemni5cm + url7 + temp200cm + " HTTP/1.1\r\n" +
                    "Host: " + host + "\r\n" +
                    "Connection: close\r\n\r\n");
 
